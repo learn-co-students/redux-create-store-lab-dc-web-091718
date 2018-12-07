fa4997cb778652126f863ed4256ecdde6d62f78d
tomgiagtz <tom.giagtzoglou@gmail.com>
2018-12-07 10:13:58 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,21 @@
 export default function createStore(reducer) {
-  // add your code here
+	let state = reducer(state, '@@INIT');
+
+	function dispatch(action) {
+		state = reducer(state, action)
+		render()
+	}
+
+	function getState() {
+		return state
+	}
+
+	return {
+		dispatch,
+		getState
+	}
 }
 
 function render() {
-  const container = document.getElementById('container');
+	const container = document.getElementById('container');
 }
